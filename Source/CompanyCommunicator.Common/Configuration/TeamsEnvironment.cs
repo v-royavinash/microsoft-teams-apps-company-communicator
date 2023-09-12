4aa21d07b0b24fb10031eaf7761a233b1ff5a2e0
Sai Venkat <98143492+gsv022@users.noreply.github.com>
2023-09-12 13:16:26 +0530

    Update TeamsEnvironment.cs

--- Source/CompanyCommunicator.Common/Configuration/TeamsEnvironment.cs
+++ Source/CompanyCommunicator.Common/Configuration/TeamsEnvironment.cs
@@ -1,4 +1,4 @@
-﻿namespace Microsoft.Teams.Apps.CompanyCommunicator.Common
+namespace Microsoft.Teams.Apps.CompanyCommunicator.Common
 {
     /// <summary>
     /// Teams environment.
@@ -18,6 +18,11 @@
         /// <summary>
         /// GCCH - https://learn.microsoft.com/en-us/office365/servicedescriptions/office-365-platform-service-description/office-365-us-government/gcc-high-and-dod
         /// </summary>
-        GCCH
+        GCCH,
+
+        /// <summary>
+        /// DOD - https://learn.microsoft.com/en-us/office365/servicedescriptions/office-365-platform-service-description/office-365-us-government/gcc-high-and-dod
+        /// </summary>
+        DOD
     }
 }
